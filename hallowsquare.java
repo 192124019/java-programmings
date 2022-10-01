ed74b4c060daef4c232dddb4bbaec280178509c0
192124019 <112462605+192124019@users.noreply.github.com>
2022-10-01 08:29:47 +0530

    Update hallowsquare.java

--- hallowsquare.java
+++ hallowsquare.java
@@ -10,7 +10,7 @@ public class hallowsquare {
 		for (int i = 1; i <=row; i++) {
 			for (int j = 1; j <= col; j++) 
 				if((i==1 || i==col) || (j==1 || j==col))
-					System.out.print("*");
+					System.out.print("$");
 				else
 					System.out.print(" ");
 			
